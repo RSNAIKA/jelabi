2949bd44b7c4aa79ded5206ec091256a16ea66a9
Abhinav Srivastava <abhisri1997@gmail.com>
2016-03-18 17:27:14 +0530

    NO Changes

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
-PATH=${PATH}:/home/abhi/android/kernel/toolchains/google/arm-eabi-4.8/bin/
+PATH=${PATH}:/home/abhi/kernel/toolchains/google/arm-eabi-4.8/bin/
 export ARCH=arm
 make cyanogenmod_wt88047_defconfig ARCH=arm CROSS_COMPILE=arm-eabi-
-make ARCH=arm CROSS_COMPILE=arm-eabi-
-make -j4
+make -j4 ARCH=arm CROSS_COMPILE=arm-eabi-
