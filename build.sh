0e3868953e82e66eff7e6fa78e2ef5a3565f3712
Abhinav Srivastava <abhisri1997@gmail.com>
2016-03-20 04:14:24 +0530

    Added Cache Suppot and Vibration Control Feature

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 PATH=${PATH}:~/toolchains/google/arm-eabi-4.8/bin/
+export USE_CCACHE=1
+export CCACHE_DIR=~/android/cache
 export ARCH=arm
 make cyanogenmod_wt88047_defconfig ARCH=arm CROSS_COMPILE=arm-eabi-
 make -j4 ARCH=arm CROSS_COMPILE=arm-eabi-
