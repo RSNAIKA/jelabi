6b3ed9ecff066a707bc9194cce7ed21c2b9a7794
Abhinav Srivastava <abhisri1997@gmail.com>
2016-03-20 07:10:41 +0530

    Added Cache Directory

--- build.sh
+++ build.sh
@@ -2,5 +2,6 @@ PATH=${PATH}:~/toolchains/google/arm-eabi-4.8/bin/
 export USE_CCACHE=1
 export CCACHE_DIR=~/android/cache
 export ARCH=arm
+export SUBARCH=arm
 make cyanogenmod_wt88047_defconfig ARCH=arm CROSS_COMPILE=arm-eabi-
 make -j4 ARCH=arm CROSS_COMPILE=arm-eabi-
